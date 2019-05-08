6553fcf31b2fa433dbfb71a9844a28363d1efa58
Pan Jie <panjie@growingio.com>
2019-05-08 19:14:54 +0800

    Fix require problem.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
+require "#{File.join(Gem.loaded_specs['dryad-core'].gem_dir, 'spec')}/spec_helper.rb"
 
 Dir[
-  "#{File.join(Gem.loaded_specs['dryad-core'].gem_dir, 'spec')}/**/*.rb"
+  "#{File.join(Gem.loaded_specs['dryad-core'].gem_dir, 'spec')}/**/*_spec.rb"
 ].each {|f| require f}
