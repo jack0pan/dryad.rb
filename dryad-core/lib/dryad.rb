114bc505c50ae933e2069baeb7ddf59956de0736
Pan Jie <panjie@growingio.com>
2019-06-03 20:12:58 +0800

    Change files.

--- dryad-core/lib/dryad.rb
+++ dryad-core/lib/dryad.rb
@@ -1,18 +1,1 @@
-require "dryad/configuration"
 require "dryad/core"
-
-module Dryad
-  class Error < StandardError; end
-
-  class << self
-    attr_accessor :configuration
-  end
-
-  def self.configuration
-    @configuration ||= Configuration.new
-  end
-
-  def self.configure
-    yield(configuration)
-  end
-end
